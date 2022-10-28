626520ff6c4c4a6ecab8aac0adb94a8861bd274d
asluchych <anatolblr@gmail.com>
2022-10-28 19:28:16 +0200

    Change secret key

--- hello.py
+++ hello.py
@@ -11,7 +11,7 @@ from flask_migrate import Migrate
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SECRET_KEY'] = 'ekaterinashulman'
 app.config['SQLALCHEMY_DATABASE_URI'] =\
     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
