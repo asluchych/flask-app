862d74ce41b75686f3c1a789e4864ee0d021565c
asluchych <anatolblr@gmail.com>
2022-11-01 18:42:29 +0100

    Fix typos

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,9 @@
-from flask import render_template, session, redirect, url_for, curent_app
-from . import main
-from .forms import NameForm
+from flask import render_template, session, redirect, url_for, current_app
 from .. import db
 from ..models import User
 from ..email import send_email
+from . import main
+from .forms import NameForm
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -16,11 +16,13 @@ def index():
             db.session.add(user)
             db.session.commit()
             session['known'] = False
-            if curent_app.config['FLASKY_ADMIN']:
-                send_email(curent_app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
+            if current_app.config['FLASKY_ADMIN']:
+                send_email(current_app.config['FLASKY_ADMIN'], 'New User',
+                           'mail/new_user', user=user)
         else:
             session['known'] = True
         session['name'] = form.name.data
         return redirect(url_for('.index'))
-    return render_template('index.html', form=form, name=session.get('name'),
+    return render_template('index.html',
+                           form=form, name=session.get('name'),
                            known=session.get('known', False))
--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from . import db
 
+
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
@@ -9,9 +10,10 @@ class Role(db.Model):
     def __repr__(self):
         return '<Role %r>' % self.name
 
+
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
