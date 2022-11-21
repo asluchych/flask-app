792f5e471729fc72f57c984e555ef9794849aa5c
asluchych <anatolblr@gmail.com>
2022-11-21 19:15:52 +0100

    Add blog posts to profile page

--- app/main/views.py
+++ app/main/views.py
@@ -15,14 +15,15 @@ def index():
         db.session.add(post)
         db.session.commit()
         return redirect(url_for('.index'))
-    posts = Post.query.order_by(Post.timestamps.desc()).all()
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
     return render_template('index.html', form=form, post=posts)
 
 
 @main.route('/user/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    return render_template('user.html', user=user)
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('user.html', user=user, posts=posts)
 
 
 @main.route('/edit-profile', methods=['GET', 'POST'])
