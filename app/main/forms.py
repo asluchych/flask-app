613c981dad83371ba93cefc66dfe64d2c0657f32
asluchych <anatolblr@gmail.com>
2022-11-22 13:37:09 +0100

    Implement rich text blog posts

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, BooleanField, SelectField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, Regexp
 from wtforms import ValidationError
+from flask_pagedown.fields import PageDownField
 from ..models import Role, User
 
 class NameForm(FlaskForm):
@@ -43,5 +44,5 @@ class EditProfileAdminForm(FlaskForm):
 
 
 class PostForm(FlaskForm):
-    body = TextAreaField('What\'s on your mind?', validators=[DataRequired()])
+    body = PageDownField('What\'s on your mind?', validators=[DataRequired()])
     submit = SubmitField('Submit')
