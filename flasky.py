937b1464ffa3d0cf0b1ccce106b5f3f6afb300e3
asluchych <anatolblr@gmail.com>
2022-12-02 10:04:36 +0100

    Minor bugfix in creating HTML version of coverage report

--- flasky.py
+++ flasky.py
@@ -47,5 +47,5 @@ def test(coverage, test_names):
         basedir = os.path.abspath(os.path.dirname(__file__))
         covdir = os.path.join(basedir, 'tmp/coverage')
         COV.html_report(directory=covdir)
-        print('HTML version: file://%s/index.html' % covidr)
+        print('HTML version: file://%s/index.html' % covdir)
         COV.erase()
